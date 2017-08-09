bbaa9bbc2e5511732ebbc8d4f645bea4dd11144c
James <jamesgood626@gmail.com>
2017-08-09 13:55:21 -0700

    added config vars

---
+++ config/prod_keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    API_KEY: process.ENV.API_KEY,
+    DOMAIN: process.ENV.DOMAIN
+}
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-// const KEYS = require('./config/keys')
-// var api_key = KEYS.API_KEY
-// var domain = KEYS.DOMAIN
-// var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
+const KEYS = require('./config/prod_keys')
+var api_key = KEYS.API_KEY
+var domain = KEYS.DOMAIN
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
 
 
 
 
 app.use(bodyParser.json())
 
-// app.post('/contact/send', (req, res) => {
-//   let data = {
-//     from: `James Good <postmaster@${domain}>`,
-//     to: 'jamesgoodwebdevelopment@gmail.com',
-//     subject: 'Hello',
-//     text: "Name: " + req.body.name + "\n\n" + "Email: " + req.body.email + "\n\n" + 
-//           "Phone Number: " + req.body.phoneNumber + "\n\n" + "Current Site: " + req.body.currentSite +"\n\n" + 
-//           "Project Information: " + req.body.projectInfo
-//   }
+app.post('/contact/send', (req, res) => {
+  let data = {
+    from: `James Good <postmaster@${domain}>`,
+    to: 'jamesgoodwebdevelopment@gmail.com',
+    subject: 'Hello',
+    text: "Name: " + req.body.name + "\n\n" + "Email: " + req.body.email + "\n\n" + 
+          "Phone Number: " + req.body.phoneNumber + "\n\n" + "Current Site: " + req.body.currentSite +"\n\n" + 
+          "Project Information: " + req.body.projectInfo
+  }
  
-//   mailgun.messages().send(data, function (error, body) {
-//     console.log(body)
-//   })
-// })
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body)
+  })
+})
 
 
 if (process.env.NODE_ENV === 'production') {
