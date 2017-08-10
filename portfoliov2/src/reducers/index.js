56352d77ff08bafb69e057bda56f83e29d32d99f
James <jamesgood626@gmail.com>
2017-08-10 13:26:48 -0700

    the actual revision forgot to build

--- portfoliov2/src/reducers/index.js
+++ portfoliov2/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import sentEmailReducer from './sentEmailReducer'
-import toggleMenuReducer from './toggleMenuReducer'
 
 export default combineReducers({
     emailSuccess: sentEmailReducer,
