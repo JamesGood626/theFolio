6292672646c2b905448de348ecd567a3484adb49
James <jamesgood626@gmail.com>
2017-08-11 00:27:38 -0700

    part4

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ app.post('/contact/send', (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  
   const path = require('path')
-  app.use(express.static(path.join(__dirname, 'client/build')))
-
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
   })
 }
 
