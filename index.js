fbac923772388d4e0271c33950ac278509a93924
James <jamesgood626@gmail.com>
2017-08-11 03:19:20 -0700

    here we go again

--- index.js
+++ index.js
@@ -30,17 +30,10 @@ app.post('/contact/send', (req, res) => {
 })
 
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+})
 
-
-if (process.env.NODE_ENV === 'production') {
-
-  app.use(express.static('client/build'))
-  
-  
-  // app.get('/*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  // })
-}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
