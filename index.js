6ac664c98d1bba023a325ef11af8763857c6afd5
James <jamesgood626@gmail.com>
2017-08-10 23:47:07 -0700

    editing express server to serve react app...

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ app.post('/contact/send', (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('portfoliov2/build'));
+  app.use(express.static('portforliov2/build'))
+
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 
