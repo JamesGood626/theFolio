0f2f5c669cdf054f285b94ffbc9a95ae1f409c7c
James <jamesgood626@gmail.com>
2017-08-11 00:00:30 -0700

    onemoretime

--- index.js
+++ index.js
@@ -28,14 +28,13 @@ app.post('/contact/send', (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('portfoliov2/build'))
+  app.use(express.static('client/build'))
 
   const path = require('path')
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'portfoliov2', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
-
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
