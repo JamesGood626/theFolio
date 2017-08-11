3e81f205026e70d5b325d422d2ad5c71fb13ba45
James <jamesgood626@gmail.com>
2017-08-11 01:32:50 -0700

    part 8

--- index.js
+++ index.js
@@ -32,16 +32,15 @@ app.post('/contact/send', (req, res) => {
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   const path = require('path')
+if (process.env.NODE_ENV === 'production') {
 
-//   app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build/*')))
   
   
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-//   })
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
