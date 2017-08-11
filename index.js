7d7ba1a8d87ebecdee0c2b0da2dbb75691375275
James <jamesgood626@gmail.com>
2017-08-11 01:20:37 -0700

    part 7

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const bodyParser = require('body-parser')
 const KEYS = require('./config/prod_keys')
 var api_key = KEYS.API_KEY
