16cec34997e9b6dd81bab6b455bca9dfca6259fd
James <jamesgood626@gmail.com>
2018-04-20 01:45:43 -0700

    .

--- client/src/Pages/About/index.js
+++ client/src/Pages/About/index.js
@@ -10,7 +10,7 @@ import moto828 from './moto-828.png'
 const Div = styled.div`
   height: 100%;
   width: 100vw;
-  background-color: limegreen;
+  background-color: purple;
   margin-bottom: 2.4rem;
 `
 
